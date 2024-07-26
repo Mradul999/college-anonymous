6badcb5fe11fbd2bf11ddb0315fa5cf1f320bd0c
Mradul999 <demorgans007@gmail.com>
2024-07-26 21:40:08 +0530

    create server

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,8 +1,16 @@
 import express from "express";
-import mongoose  from "mongoose";
-const app=express();
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+const app = express();
+dotenv.config();
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => console.log("Database connected successfully"))
+  .catch((err) => console.log(err));
 
 app.use(express.json());
 
-
+app.listen(process.env.PORT||4000,()=>{
+    console.log(`Server running on port ${process.env.PORT}`);
+})
