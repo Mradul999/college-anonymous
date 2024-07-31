b0b560000a639712151d67af5234e91692a24534
Mradul999 <demorgans007@gmail.com>
2024-07-31 19:37:22 +0530

    index.js

--- api/index.js
+++ api/index.js
@@ -1,24 +1,30 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 import authRoute from "./routes/auth.js";
-import postRoute from "./routes/post.js"
-import commentRoute from "./routes/comment.js"
-import userRoute from "./routes/user.js"
+import postRoute from "./routes/post.js";
+import commentRoute from "./routes/comment.js";
+import userRoute from "./routes/user.js";
 
 const app = express();
+app.use(
+  cors({
+    origin: "https://silent-campus.vercel.app",
+    credentials: true,
+  })
+);
 app.use(express.json());
 dotenv.config();
 mongoose
   .connect(process.env.DATABASE_URL)
   .then(() => console.log("Database connected successfully"))
-  .catch((err) => console.log(err));  
+  .catch((err) => console.log(err));
 
+app.use("/api/auth", authRoute);
+app.use("/api/post", postRoute);
+app.use("/api/comment", commentRoute);
+app.use("/api/user", userRoute);
 app.listen(process.env.PORT || 4000, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
-
-app.use("/api/auth", authRoute);
-app.use("/api/post",postRoute);
-app.use("/api/comment",commentRoute);
-app.use("/api/user",userRoute);
