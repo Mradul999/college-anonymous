55e502aab63764a33fd0155a6b6a04aaec39a7a3
Mradul999 <demorgans007@gmail.com>
2024-08-01 14:09:31 +0530

    fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import dotenv from "dotenv"
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://silent-campus-backend.vercel.app",
+        target: process.env.VITE_API_URL,
         changeOrigin: true,
         secure: false,
       },
