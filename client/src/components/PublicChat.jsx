75402a640d5cfbc7b696aeeb65b54a4231b1ed19
Mradul999 <demorgans007@gmail.com>
2025-03-26 12:13:15 +0530

    commit

--- client/src/components/PublicChat.jsx
+++ client/src/components/PublicChat.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import { useSelector } from "react-redux";
 
-const socket = io("https://college-anonymous.onrender.com");
+const socket = io(import.meta.env.VITE_BACKEND_URL);
 // console.log(import.meta.env.VITE_BACKEND_URL);
 
 const PublicChat = () => {
